c9c4346eedc20190024733c78ca135bafd5113fb
tmh0214 <smyy0214@163.com>
2017-06-21 17:36:33 +0800

    first commit

---
+++ zeus/src/main/java/cn.itcast.zeus/ServletInitializer.java
@@ -0,0 +1,14 @@
+package cn.itcast.zeus;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Created by tmh0214 on 17/6/21.
+ */
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ZeusApplication.class);
+    }
+}
---
+++ zeus/src/main/java/cn.itcast.zeus/ZeusApplication.java
@@ -0,0 +1,17 @@
+package cn.itcast.zeus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by tmh0214 on 17/6/21.
+ */
+@SpringBootApplication
+public class ZeusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeusApplication.class, args);
+    }
+}
+
+
